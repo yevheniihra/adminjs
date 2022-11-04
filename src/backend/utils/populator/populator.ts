babcf6158b11eb7d97daaf33e94a4435643b4ae8
yevhenii-hra <115156946+yevhenii-hra@users.noreply.github.com>
2022-11-04 13:36:06 +0200

    Update populator.ts
    (cherry picked from commit b48ea9a7796a4d797c5208825da951a708c7220b)

--- src/backend/utils/populator/populator.ts
+++ src/backend/utils/populator/populator.ts
@@ -1,16 +1,14 @@
 import BaseRecord from '../../adapters/record/base-record'
 import { populateProperty } from './populate-property'
-import { ActionContext } from '../../actions'
 
 /**
  * @load ./populator.doc.md
  * @param {Array<BaseRecord>} records
- * @param context
  * @new In version 3.3
  */
 export async function populator(
   records: Array<BaseRecord>,
-  context?:ActionContext,
+  excludePopulate: Array<string> = [],
 ): Promise<Array<BaseRecord>> {
   if (!records || !records.length) {
     return records
@@ -18,10 +16,15 @@ export async function populator(
   const resourceDecorator = records[0].resource.decorate()
   const allProperties = Object.values(resourceDecorator.getFlattenProperties())
 
-  const references = allProperties.filter((p) => !!p.reference())
+  const lowercaseExclude = excludePopulate.map((str) => str.toLowerCase())
+
+  const references = allProperties.filter(
+    (p) => !!p.reference()
+      && !lowercaseExclude.includes(p.name().toLowerCase()),
+  )
 
   await Promise.all(references.map(async (propertyDecorator) => {
-    await populateProperty(records, propertyDecorator, context)
+    await populateProperty(records, propertyDecorator)
   }))
   return records
 }
